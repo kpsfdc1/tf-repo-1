024e9f61c7e4a10bb27563f728f48df636eba186
root <root@ip-172-31-5-203.ec2.internal>
2024-10-07 03:55:53 +0000

    main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+terraform {
+	backend "s3" {
+		bucket = "tf-state-buk-cis"
+		region = "us-east-1"
+		key = "terraform.tfstate"
+	}
+}
+
+resource "aws_instance" "tf_inst" {
+	ami = "ami-0a0e5d9c7acc336f1"
+	instance_type = "t2.micro"
+	tags = {
+		Name = "new-tf-inst"
+	}
+}
